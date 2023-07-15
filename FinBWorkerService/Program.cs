4477b60d769888e320bcc46ecafc29150239144d
OkothPius <orukopius8@gmail.com>
2023-07-16 00:09:52 +0300

    feat: worker service

---
+++ FinBWorkerService/Program.cs
@@ -0,0 +1,10 @@
+using FinBWorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
